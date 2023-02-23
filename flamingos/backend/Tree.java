bacec38ef39bc5b51cf58239dbf7b5389b4163f8
Jack Maumus <jpmmaumus@gmail.com>
2023-02-23 14:31:55 -0600

    Update Tree.java

--- flamingos/backend/Tree.java
+++ flamingos/backend/Tree.java
@@ -1,35 +1,46 @@
 public class Tree {
 
+    //Returns the size of a given tree
     public static int sizeOfTree(Tree tree) {
 
         return 0;
     }
 
+
+    //Returns the height of a given tree
     public static int heightOfTree(Tree tree) {
 
         return -1;
     }
 
+
+    //Returns whether a given tree is empty or not
     public static boolean isEmpty(Tree tree) {
 
         return false;
     }
 
+    //Inserts a node into a tree
     public static void insertNode(Tree tree, Node node) {
 
         return;
     }
 
+
+    //Deletes node from tree
     public static void deleteNode(Tree tree, Node node) {
 
         return;
     }
 
+    //Searches tree for a node
     public static int searchTree(Tree tree, Node node) {
 
         return -1;
     }
 
+
+    //Deletes a tree along with its nodes
     public static void clearTree(Tree tree) {
 
         return;
