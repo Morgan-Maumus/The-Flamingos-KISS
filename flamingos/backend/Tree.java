80342f02ae06c30c05c2fdd6814b90e3423e8ac0
Jack Maumus <jpmmaumus@gmail.com>
2023-02-23 13:52:24 -0600

    Backend Skeleton

---
+++ flamingos/backend/Tree.java
@@ -0,0 +1,38 @@
+public class Tree {
+
+    public static int sizeOfTree(Tree tree) {
+
+        return -1;
+    }
+
+    public static int heightOfTree(Tree tree) {
+
+        return -1;
+    }
+
+    public static boolean isEmpty(Tree tree) {
+
+        return false;
+    }
+
+    public static void insertNode(Tree tree, Node node) {
+
+        return;
+    }
+
+    public static void deleteNode(Tree tree, Node node) {
+
+        return;
+    }
+
+    public static int searchTree(Tree tree, Node node) {
+
+        return -1;
+    }
+
+    public static void clearTree(Tree tree) {
+
+        return;
+    }
+
+}
