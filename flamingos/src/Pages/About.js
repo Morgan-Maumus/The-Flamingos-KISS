753d959bc1adfb078cafde10e515fd6cdfcd8b7e
MALANA S FUENTES <mvu21@lsu.edu>
2023-03-15 13:22:49 -0500

    edited app.css for about page, functions added to About.js
    Signed-off-by: MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>

--- flamingos/src/Pages/About.js
+++ flamingos/src/Pages/About.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../App.css';
 
+function Background(){
+    return(
+
+    );
+}
+const rootBG = ReactDOM.createRoot(document.getElementById('root'))
+rootBG.render(<Background/>);
+function Team(){
+    return(
+
+    );
+}
+const rootT = ReactDOM.createRoot(document.getElementById('root'))
+rootT.render(<Team/>);
 
 function About(){
-    
-    
-      
+
     return (
-       <div className = 'about'>
-            
-            <p>Hello</p>
-       </div>
-    )
+        <div>
+            <Team />
+            <Background />
+        </div>
+    );
 }
-export default About
+export default About;
