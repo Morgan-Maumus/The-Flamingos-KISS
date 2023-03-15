43e8cedae4ddba93dcdac0935a7ffbfba38d3573
MALANA S FUENTES <mvu21@lsu.edu>
2023-03-15 15:11:21 -0500

    Reverted About.js
    Signed-off-by: MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>

--- flamingos/src/Pages/About.js
+++ flamingos/src/Pages/About.js
@@ -1,33 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import '../App.css';
 
-function Background(){
-    return(
-        <>
-            <p>meow</p>
-        </>
-    );
-}
-const rootBG = ReactDOM.createRoot(document.getElementById('root'))
-rootBG.render(<Background/>);
-function Team(){
-    return(
-<>
-<p>meow</p>
-</>
-    );
-}
-const rootT = ReactDOM.createRoot(document.getElementById('root'))
-rootT.render(<Team/>);
 
 function About(){
 
+
+
     return (
-        <div>
-            <Team />
-            <Background />
+                <div className = 'about'>
+
+            <p>Hello</p>
         </div>
-    );
+    )
 }
 export default About;
