94511864d81ad3366b56ea1da23d1c49b6400baf
MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>
2023-03-17 14:52:48 -0500

    edits to about page; Id cards Made ish
    Signed-off-by: MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>

--- flamingos/src/Pages/About.js
+++ flamingos/src/Pages/About.js
@@ -3,26 +3,80 @@ import '../App.css';
 
 function Header() {
     return (
-        <div className='container' id='kHead'>
+        <div className='container'>
             <div className='kissHeader'> K.I.S.S.
                 <div className='logoText'>
                     <u>Meet the Keep It Simple, Stupid Team!</u>
                 </div>
             </div>
+            <div className='logoImg'>
+            <img src= 'Images/logo.png' alt='logo'/>
+            </div>
         </div>
     )
 }
 function Body(){
+
     return(
+        <>
         <div className='row'>
             <div className='columnImg'>
                 <div className='card' width='100%'>
+                    <img herf='Images/jackjack.jpg' alt='idk'/>
                     <div className='container'>
                         <p>Jack</p>
                     </div>
                 </div>
             </div>
+
+            <div className='columnImg'>
+                <div className='card' width='100%'>
+                    <div className='container'>
+                        <img src='src/Images/nat.png' alt='why'/>
+                        <p>Lana</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='columnImg'>
+                <div className='card' width='100%'>
+                    <div className='container'>
+                        <img herf='Images/jackjack.jpg' alt='this'/>
+                        <p>Shelby</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div className='row'>
+            <div className='columnImg'>
+                    <div className='card' width='100%'>
+                        <img herf='Images/jackjack.jpg' alt='hates'/>
+                        <div className='container'>
+                            <p>Savannah</p>
+                        </div>
+                    </div>
+                </div>
+
+            <div className='columnImg'>
+                    <div className='card' width='100%'>
+                        <div className='container'>
+                            <img herf='Images/jackjack.jpg' alt='me'/>
+                            <p>Nat</p>
+                        </div>
+                    </div>
+                </div>
+
+            <div className='columnImg'>
+                    <div className='card' width='100%'>
+                        <div className='container'>
+
+                            <img herf='Images/jackjack.jpg' alt=':('/>
+                            <p>Phil</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </>
     )
 }
 function About(){
