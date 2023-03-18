09e41620341586a07047b5362fc95d56c25d040f
Jack Maumus <jpmmaumus@gmail.com>
2023-03-18 14:09:39 -0500

    hyperlink hover text

--- flamingos/src/Components/Navigation.js
+++ flamingos/src/Components/Navigation.js
@@ -11,13 +11,13 @@ function Navigation(){
         <div className = "nav-bar"> 
             
             <div className="nav-left">
-                <a href="./About" className='nav-links nav-left'><img id='logo' src={logo}></img></a>
+                <a href="./About" className='nav-links nav-left'><img title='Go to the About Page' id='logo' src={logo}></img></a>
             </div>
             <div className="nav-center">
-                <a href="./" className = "nav-links nav-center" classname='center-padding'><img id='home' src={home}></img></a>
+                <a href="./" className = "nav-links nav-center" classname='center-padding'><img title='Go to the Home (Roots) Page' id='home' src={home}></img></a>
             </div>
             <div className="nav-right">    
-                <a href="./Signin" className='nav-links nav-right'><img id='signin' src={signin}></img></a>
+                <a href="./Signin" className='nav-links nav-right'><img title='Go to the User Account Page' id='signin' src={signin}></img></a>
             </div>
         </div>
     )
