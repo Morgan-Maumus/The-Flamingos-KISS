866508a3524667ab3e2912ada95a8e8846e3d608
MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>
2023-03-18 19:16:56 -0500

    edits to about page
    Signed-off-by: MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>

--- flamingos/src/Components/Navigation.js
+++ flamingos/src/Components/Navigation.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import {Link} from "react-router-dom";
-import logo from "../Images/logo.png";
-import home from "../Images/home.png";
-import signin from "../Images/signin.jpg";
 
 
 function Navigation(){
@@ -11,13 +8,13 @@ function Navigation(){
         <div className = "nav-bar"> 
             
             <div className="nav-left">
-                <a href="./About" className='nav-links nav-left'><img title='Go to the About Page' id='logo' src={logo}></img></a>
+                <Link className="nav-links nav-left"  to ="./About">About</Link>
             </div>
             <div className="nav-center">
-                <a href="./" className = "nav-links nav-center" classname='center-padding'><img title='Go to the Home (Roots) Page' id='home' src={home}></img></a>
+                <Link className="nav-links nav-center" classname= "center-padding" to ="./The-Flamingos-Kiss">Home</Link>
             </div>
             <div className="nav-right">    
-                <a href="./Signin" className='nav-links nav-right'><img title='Go to the User Account Page' id='signin' src={signin}></img></a>
+                <Link className="nav-links nav-right"to ="./Signin">Log In</Link>
             </div>
         </div>
     )
--- flamingos/src/Pages/About.js
+++ flamingos/src/Pages/About.js
@@ -1,90 +1,94 @@
 import React from 'react';
 import '../App.css';
+import jackjack from '../Images/jackjack.jpg';
+import logo from '../Images/logo.png';
+import nat from '../Images/nat.png';
+import Shelbs from '../Images/Shelbs.jpg';
+import lana from '../Images/lana.jpg';
+import sav from '../Images/sav.jpg';
+import Phil from '../Images/Phil.jpg';
+
 
 function Header() {
     return (
+        <>
         <div className='container'>
+            <div className='bgImg'/>
             <div className='kissHeader'> K.I.S.S.
                 <div className='logoText'>
                     <u>Meet the Keep It Simple, Stupid Team!</u>
                 </div>
             </div>
-            <div className='logoImg'>
-            <img src= 'Images/logo.png' alt='logo'/>
-            </div>
         </div>
+        </>
     )
 }
 function Body(){
 
     return(
         <>
-        <div className='row'>
+            <div className='row'>
             <div className='columnImg'>
                 <div className='card' width='100%'>
-                    <img herf='Images/jackjack.jpg' alt='idk'/>
+                    <img src= { jackjack } alt='Jack' width='100%' height='auto'/>
                     <div className='container'>
                         <p>Jack</p>
                     </div>
                 </div>
             </div>
-
             <div className='columnImg'>
                 <div className='card' width='100%'>
+                    <img src= { lana } alt='Lana' width='100%'/>
                     <div className='container'>
-                        <img src='src/Images/nat.png' alt='why'/>
                         <p>Lana</p>
                     </div>
                 </div>
             </div>
-
             <div className='columnImg'>
                 <div className='card' width='100%'>
+                    <img src= { Shelbs } alt='Shelby' width='100%' height='auto'/>
                     <div className='container'>
-                        <img herf='Images/jackjack.jpg' alt='this'/>
                         <p>Shelby</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div className='row'>
-            <div className='columnImg'>
+            <div className='row'>
+                <div className='columnImg'>
                     <div className='card' width='100%'>
-                        <img herf='Images/jackjack.jpg' alt='hates'/>
+                        <img src= { sav } alt='Savannah' width='100%' height='auto'/>
                         <div className='container'>
                             <p>Savannah</p>
                         </div>
                     </div>
                 </div>
-
-            <div className='columnImg'>
+                <div className='columnImg'>
                     <div className='card' width='100%'>
+                        <img src= { nat } alt='Nat' width='100%' height='auto'/>
                         <div className='container'>
-                            <img herf='Images/jackjack.jpg' alt='me'/>
                             <p>Nat</p>
                         </div>
                     </div>
                 </div>
-
-            <div className='columnImg'>
+                <div className='columnImg'>
                     <div className='card' width='100%'>
+                        <img src= { Phil } alt='Phil' width='100%' height='auto'/>
                         <div className='container'>
-
-                            <img herf='Images/jackjack.jpg' alt=':('/>
                             <p>Phil</p>
                         </div>
                     </div>
                 </div>
-        </div>
+            </div>
         </>
     )
 }
+
 function About(){
     return (
         <>
             <Header/>
             <Body/>
         </>
-            )
+    )
 }
 export default About;
