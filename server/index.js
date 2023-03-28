3bedab77d762c4aaab0b81174bc8478939defc6a
MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>
2023-03-28 08:35:16 -0500

    working on database comm
    Signed-off-by: MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -2,13 +2,25 @@ const path = require('path');
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const {MongoClient} = require("mongodb");
+const uri = process.env.MONGODB_URI;
 
 app.use(express.static(path.resolve(__dirname, '../flamingos/build')));
 
 app.get('*', (req,res) =>{
     res.sendFile((path.resolve(__dirname,'../flamingos/build', 'index.html')));
 })
-  
+
+/*app.get('/api/userID', async function (req,res){
+    const flamingos = new MongoClient(uri, {useUnifiedTopology : true});
+    try {
+        await flamingos.connect();
+        const database = flamingos.db('flamingodb');
+        const collection = database.addCollection('userID');
+    }
+    catch ()
+})*/
+
 app.listen(PORT, () => {
 console.log(`Server listening on ${PORT}`);
 });
