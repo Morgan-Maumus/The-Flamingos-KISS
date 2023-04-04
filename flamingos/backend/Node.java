a5ead8fa2f9aa2e6131daeec983665eaf869ecda
MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>
2023-04-03 19:11:43 -0500

    Revert "commiting changes to heroku master"
    This reverts commit 6fa37b4ce62e97a273e91eb6f55e8971f98fe9e5.

--- flamingos/backend/Node.java
+++ flamingos/backend/Node.java
@@ -1,95 +1,111 @@
+import java.lang.reflect.Array;
+import java.util.LinkedList;
 import java.util.ArrayList;
 
 public class Node {
-    private int nodesCompleted = 0;
-    private ArrayList<Node> nodeChildren = new ArrayList<>();
-    private String nodeTitle;
+    //constructor for the node
+        //if the node is completed, add 1 to the number of nodes completed
+        int nodesCompleted = 0;
+        boolean isComplete;
+        for(i=0; i<nodeChildren.length; i++){
+         nodesCompleted ++;
+        }
+        
+    //determines if a node is the root/ main task
+    public static boolean isRoot(Node node){
+        private boolean isRoot;
+        if(nodeParent == null){
+            isRoot = true;
+        }
+        else{
+            isRoot = false;
+        }
+    }
+
+    //variable that contains the node's children
+    private static ArrayList nodeChildren = new ArrayList<String>(); 
+    for(i=0; i<node.length; i++){
+        nodeChildren.add(node(i+1));
+    }
+
+    //variable for the title of the node
+    private String nodeTitle = "Node Title";
+
+    //variable for the node description
     private String nodeDescription;
-    public boolean isComplete = false;
-    private Node nodeParent = null;
 
-    // Constructor for the node class
-    public Node(String title) {
-        this.nodeTitle = title;
+    //variable for the node parent
+    //private Node nodeParent = Node(i-1);
+    private Node nodeParent = new node(new Runnable(){
+        public void run(){
+            if (Node(i-1) < 0){
+                nodeParent = null;
+            }
+            else{
+                nodeParent = Node(i-1);
+            }
+        }
+    });
+
+    //gets the title of the node
+    public static String getTitle(Node node){
+        return node.nodeTitle;
     }
 
-    // Method to add a child node to this node's list of children
-    public void addChild(Node child) {
-        nodeChildren.add(child);
-        child.setParent(this);
+    //sets the title of the node
+    public static String setTitle(Node node){
+        String title;
+        for(int i=0; i<nodeChildren.length; i++){
+            if (node = nodeChildren(i)){
+                title = getParent(nodeChildren(i));
+                
+            }
+        }
+        return title;
     }
 
-    // Method to get the number of completed nodes
-    public int getNodesCompleted() {
-        return nodesCompleted;
+    //returns the node's description
+    public static String getDescription(Node node){
+        return node.nodeDescription;
     }
 
-    // Method to set the number of completed nodes
-    public void setNodesCompleted(int nodesCompleted) {
-        this.nodesCompleted = nodesCompleted;
+    //sets the nodes description
+    public static void setDescription(Node node){
+    //sets the node's description
+        getDescription(node);
     }
 
-    // Method to check if a node is the root/main task
-    public boolean isRoot() {
-        if (nodeParent == null) {
+    //returns the node's children
+    public static ArrayList getChildren(Node node){
+        return nodeChildren;
+    }
+
+    //deletes a node's child
+    public static void deleteChild(Node child, Node parent){
+       for(i=0; i<nodeChildren.length; i++){
+              if(nodeChildren(i) == child){
+                nodeChildren.remove(i);
+              }
+         }
+    }
+
+    //returns whether a node has children
+    public static boolean hasChild(Node node){
+        if(nodeChildren.length > 0){
             return true;
-        } else {
+        }
+        else{
             return false;
         }
     }
 
-    // Method to get the title of the node
-    public String getTitle() {
-        return nodeTitle;
-    }
-
-    // Method to set the title of the node
-    public void setTitle(String title) {
-        this.nodeTitle = title;
-    }
-
-    // Method to get the node's description
-    public String getDescription() {
-        return nodeDescription;
-    }
-
-    // Method to set the node's description
-    public void setDescription(String description) {
-        this.nodeDescription = description;
-    }
-
-    // Method to get the node's children
-    public ArrayList<Node> getChildren() {
-        return nodeChildren;
-    }
-
-    // Method to delete a node's child
-    public void deleteChild(Node child) {
-        nodeChildren.remove(child);
-    }
-
-    // Method to check if a node has children
-    public boolean hasChildren() {
-        return !nodeChildren.isEmpty();
-    }
-
-    // Method to get the node's parent
-    public Node getParent() {
+    //returns the node's parents
+    public static Node getParent(Node node){
         return nodeParent;
     }
-
-    // Method to set the node's parent
-    public void setParent(Node parent) {
-        this.nodeParent = parent;
-    }
-
-    // Method to set a node as complete
-    public void setComplete() {
-         isComplete = true;
-    }
-
-    // Method to check if a node is complete
-    public boolean isComplete() {
-        return isComplete;
-    }
+    
+    //Sets a node as complete
+    public void setComplete()
+    {
+        isComplete = true;
 }
