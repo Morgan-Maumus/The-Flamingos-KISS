88a5466c5d38c05fa448d559b17c076e010d6cee
MALANA S FUENTES <105810456+ChaosxPixie@users.noreply.github.com>
2023-04-04 07:54:47 -0500

    main edits
    working out main

--- flamingos/backend/Main.java
+++ flamingos/backend/Main.java
@@ -3,8 +3,6 @@ import java.io.StreamCorruptedException;
 
 public class Main {
     public static void main(String[] args) {
-        // create new obj
-        // newobj.set
-        // system out obj.get
+
     }
 }
